b51f8fa3f90b0424b8db7a4267fea23be68a65c0
Jose Velasco <jose.juarez.velasco@gmail.com>
2023-04-06 00:58:45 -0700

    Resolved issue caused by missing Main.fxml and adjusted paths in main.java
    Added basic gitignore to avoid pushing unessisary files and senstive info

--- team-04-05/src/application/Main.java
+++ team-04-05/src/application/Main.java
@@ -3,11 +3,8 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
+import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
@@ -15,9 +12,9 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		
 		try {
-			BorderPane root = new BorderPane();
+			HBox root = (HBox)FXMLLoader.load(getClass().getClassLoader().getResource("view/Main.fxml"));
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getClassLoader().getResource("css/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			primaryStage.setTitle("test program");
