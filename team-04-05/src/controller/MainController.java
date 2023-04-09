9539842953915a4bce5a79d61093ab89d8f3d11a
Lawton Fong <57239569+lawtonfong@users.noreply.github.com>
2023-04-08 21:00:38 -0700

    data entry in main

--- team-04-05/src/controller/MainController.java
+++ team-04-05/src/controller/MainController.java
@@ -1,10 +1,12 @@
 package controller;
 
+import javafx.fxml.FXML;
+
 public class MainController {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
-
+	
 }
